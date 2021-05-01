792e00cb968e920435a3eb1d5c39f9b75c817909
Luis Barrera <luisbarrera5662@gmail.com>
2021-05-01 02:04:23 -0600

    se ha terminado el guardado de citas y su visualizacion en pacientes

--- js/perfiles/pacientes.js
+++ js/perfiles/pacientes.js
@@ -100,12 +100,89 @@ function modificarpaciente() {
 
 }
 
-function guardarcita(){
-    var hora=document.querySelector('#hora').value
-    var dato=document.querySelector('#fecha').value
+function guardarcita() {
+    var id = sessionStorage.ID
+    var hora = document.querySelector('#hora').value
+    var dato = document.querySelector('#fecha').value
     var resFecha = dato.split("-")
     var reversedFecha = resFecha.reverse()
-    var fecha=reversedFecha.join('-')
-    var motivo=document.querySelector('#motivo').value
-    console.log(hora+"  "+fecha+"  "+motivo)
+    var fecha = reversedFecha.join('-')
+    var motivo = document.querySelector('#motivo').value
+
+    var objeto = {
+        'idpaciente': id,
+        'hora': hora,
+        'fecha': fecha,
+        'motivo': motivo
+    }
+    console.log(objeto)
+
+
+    fetch('https://backend-202010223.herokuapp.com/registrarcita', {
+        method: 'POST',
+        body: JSON.stringify(objeto),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        }
+    })
+        .then(res => res.json())
+        .catch(err => {
+            console.error('Error:', err)
+            alert("Ocurrio un error, ver la consola")
+        })
+        .then(response => {
+            alert(response.Mensaje)
+            vercitas()
+        })
+
+
+}
+
+function vercitas() {
+    console.log(sessionStorage.nombre)
+    document.querySelector('#nombrepaciente').innerHTML=sessionStorage.nombre
+    var id = sessionStorage.ID
+    var tablapacientes = document.querySelector('#tcitasactuales')
+    var tablapacientes2 = document.querySelector('#tcitashistorial')
+    var cadena = ''
+    var cadena2 = ''
+
+    fetch('https://backend-202010223.herokuapp.com/mostrarcitas', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        }
+    })
+        .then(res => res.json())
+        .catch(err => {
+            console.error('Error:', err)
+            alert("Ocurrio un error, ver la consola")
+        })
+        .then(response => {
+            response.forEach(element => {
+                if (element.idpaciente == id && (element.estado=="Pendiente"||element.estado=="Aceptada")) {
+                    cadena += `<tr>
+                      <td>${element.fecha}</td>
+                      <td>${element.hora}</td>
+                      <td>${element.motivo}</td>
+                      <td>${element.doctor}</td>
+                      <td>${element.estado}</td>
+                      </tr>`
+                }
+                if (element.idpaciente == id && (element.estado=="Rechazada"||element.estado=="Completada")) {
+                    cadena2 += `<tr>
+                      <td>${element.fecha}</td>
+                      <td>${element.hora}</td>
+                      <td>${element.motivo}</td>
+                      <td>${element.doctor}</td>
+                      <td>${element.estado}</td>
+                      </tr>`
+                }
+
+            });
+            tablapacientes.innerHTML = cadena
+            tablapacientes2.innerHTML = cadena2
+        })
 }
